e4136b8aba67f4b78b5e8750cd0d728a71d054ed
EmmanuelNdo <137494060+EmmanuelNdo@users.noreply.github.com>
2023-07-19 14:40:34 +0200

    dev: update

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
-import * as echarts from "./node_modules/echarts/dist/echarts.esm.js";
+(async function () {
+  const response = await fetch("./repositories.json");
+  const json = await response.json();
+  console.log("json: ", json);
 
-var chartDom = document.querySelector("div.diagram");
-if (chartDom === null) {
-  throw new Error("div.diagram not found");
-}
-var myChart = echarts.init(chartDom, "dark");
-var option;
+  var chartDom = document.querySelector("div.diagram");
+  if (chartDom === null) {
+    throw new Error("div.diagram not found");
+  }
+  var myChart = echarts.init(chartDom, "dark");
+  var option;
 
-option = {
-  xAxis: {
-    type: "category",
-    data: [
-      "d3",
-      "Chartjs",
-      "echarts",
-      "recharts",
-      "react-flow",
-      "plotly.js",
-      "charts",
-      "chartist",
-      "apexcharts.js",
-      "highcharts",
-    ],
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      data: [106, 61.2, 55.8, 20.7, 16.6, 15.8, 14.8, 13.2, 12.7, 11.4],
-      type: "bar",
+  option = {
+    xAxis: {
+      type: "category",
+      data: [
+        "d3",
+        "Chartjs",
+        "echarts",
+        "recharts",
+        "react-flow",
+        "plotly.js",
+        "charts",
+        "chartist",
+        "apexcharts.js",
+        "highcharts",
+      ],
     },
-  ],
-};
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [106, 61.2, 55.8, 20.7, 16.6, 15.8, 14.8, 13.2, 12.7, 11.4],
+        type: "bar",
+      },
+    ],
+  };
 
-option && myChart.setOption(option);
+  option && myChart.setOption(option);
+})();
