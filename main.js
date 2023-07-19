ce90a1727d80d9739d98d5936d697318cc0e10ee
EmmanuelNdo <137494060+EmmanuelNdo@users.noreply.github.com>
2023-07-19 09:43:10 +0200

    dev: type module + ouverture serveur

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
-console.log("Start the bar diagram");
-var a = 34;
-console.log("a: ", a);
+import * as echarts from "echarts";
+
+var chartDom = document.querySelector("div.diagram");
+if (charDom === null) {
+  throw new Error("div.diagram not found");
+}
+var myChart = echarts.init(chartDom, "dark");
+var option;
+
+option = {
+  xAxis: {
+    type: "category",
+    data: [
+      "d3",
+      "Chartjs",
+      "echarts",
+      "recharts",
+      "react-flow",
+      "plotly.js",
+      "charts",
+      "chartist",
+      "apexcharts.js",
+      "highcharts",
+    ],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      data: [106, 61.2, 55.8, 20.7, 16.6, 15.8, 14.8, 13.2, 12.7, 11.4],
+      type: "bar",
+    },
+  ],
+};
+
+option && myChart.setOption(option);
