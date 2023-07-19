39e1426aae48f3aab9a2a029b258757fb3e9836a
EmmanuelNdo <137494060+EmmanuelNdo@users.noreply.github.com>
2023-07-19 16:56:35 +0200

    doc: readme v2

--- README.md
+++ README.md
@@ -4,18 +4,18 @@ Shows a list of dataviz library on github
 
 ## Install
 
-'''
+```
 git clone https://github.com/jlg-formation/dtv-july-2023.git github-charts
 cd github-charts
 npm install
 npx serve
-'''
+```
 
 Open a browser and go to
 
-'''
+```
 http://localhost:3000
-'''
+```
 
 ## Author
 
--- main.js
+++ main.js
@@ -3,23 +3,43 @@
   const json = await response.json();
 
   const repositories = json.items;
-  console.log("repositories: ", repositories);
 
-  const repos = repositories.map((repository) => ({
+  const filterRepos = repositories.filter(
+    (repo) =>
+      ![
+        "GitHub-Chinese-Top-Charts",
+        "helm",
+        "data-science",
+        "umami",
+        "tui.editor",
+        "charts",
+        "analytics",
+        "sampler",
+      ].includes(repo.name)
+  );
+
+  const repos = filterRepos.map((repository) => ({
     name: repository.name,
     stars: repository.stargazers_count,
   }));
-
-  ///console.log("json: ", json);
+  console.log("repositories: ", repositories);
 
   var chartDom = document.querySelector("div.diagram");
   if (chartDom === null) {
     throw new Error("div.diagram not found");
   }
   var myChart = echarts.init(chartDom, "vintage");
-  var option;
+  var option = {
+    grid: {
+      top: 10,
+      bottom: 50,
+      left: 200,
+      right: 30,
+    },
+    axisLabel: {
+      color: "black",
+    },
 
-  option = {
     yAxis: {
       type: "category",
       data: repos.map((r) => r.name).reverse(),
@@ -39,5 +59,5 @@
     ],
   };
 
-  option && myChart.setOption(option);
+  myChart.setOption(option);
 })();
